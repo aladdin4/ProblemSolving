646d5bb83407665bdc16947d2facb65ce7684816
A. Aladdin <aladdin.fullstack@gmail.com>
2025-06-29 18:36:57 +0300

    Solved #20 and worked on #21

---
+++ LeetCode-Easy/#0020.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode_Easy
+{
+    public class _0020
+    {
+
+        //        20. Valid Parentheses
+        //Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+        //An input string is valid if:
+        //Open brackets must be closed by the same type of brackets.
+        //Open brackets must be closed in the correct order.
+        //Every close bracket has a corresponding open bracket of the same type.
+
+        public bool IsValid(string s)
+        {
+            var pairs = new Dictionary<char, char>()
+            {
+                ['('] = ')',
+                ['{'] = '}',
+                ['['] = ']'
+
+            };
+            var openedBrackets = new List<char>();
+            for (int i = 0; i < s.Length; i++)
+            {
+                //opening
+                if (pairs.ContainsKey(s[i]))
+                {
+                    //adding to the opened brackets list
+                    openedBrackets.Add(s[i]);
+                }
+                else
+                {
+                    //closing without opened one: fails
+                    if (openedBrackets.Count == 0)
+                    {
+                        Console.WriteLine("failed because closing before opening");
+                        return false;
+                    }
+                    //closing with opened one: check if they match. If not: fails
+                    if (pairs[openedBrackets[openedBrackets.Count - 1]] != s[i])
+                    {
+                        Console.WriteLine("failed because closing one other than the opened");
+                        return false;
+                    }
+                    else
+                    {
+                        //closing with opened one: remove from the opened brackets list
+                        openedBrackets.RemoveAt(openedBrackets.Count - 1);
+                    }
+                }
+            }
+            Console.WriteLine("passed");
+            if (openedBrackets.Count > 0)
+            {
+                Console.WriteLine("failed because leaving opened without close");
+
+                return false;
+            }
+            return true;
+        }
+
+        public void test()
+        {
+            IsValid("()");
+            IsValid("()[]{}");
+            IsValid("(]");
+            IsValid("([])");
+        }
+    }
+}
---
+++ LeetCode-Easy/#0021.cs
@@ -0,0 +1,90 @@
+﻿
+namespace LeetCode_Easy
+{
+    public class _0021
+    {
+        //                   21. Merge Two Sorted Lists
+        //You are given the heads of two sorted linked lists list1 and list2.
+        //Merge the two lists into one sorted list.The list should be made by splicing together the nodes of the first two lists.
+        //Return the head of the merged linked list.
+        public ListNode MergeTwoLists(ListNode list1, ListNode list2)
+        {
+            var result = new ListNode();
+            int count = 3;
+            while (list1 != null && list2 != null && count > 0)
+            {
+                Console.WriteLine("Starting the next iteration with: " + list1.val + "," + list2.val);
+                if (list1.val < list2.val)
+                {
+                    Console.WriteLine("list1.val < list2.val");
+                    result.next = list1;
+                    list1 = list1.next; //iterate 1 step in the bigger list. (= popping it)
+                    result = list1;
+
+                }
+                else
+                {
+                    Console.WriteLine("list1.val >= list2.val");
+                    result.next = list2;
+                    list2 = list2.next; //iterate 1 step in the bigger list. (= popping it)
+                    result = list2;
+                }
+                count--;
+            }
+            return result;
+        }
+
+                                         //why this works and mine wouldn't?
+        public ListNode MergeTwoListsPerfect(ListNode list1, ListNode list2)
+        {
+            ListNode dummy = new ListNode(0);
+            ListNode op = dummy;
+
+            while (list1 != null && list2 != null)
+            {
+                if (list1.val <= list2.val)
+                {
+                    op.next = list1;
+                    list1 = list1.next;
+                }
+                else
+                {
+                    op.next = list2;
+                    list2 = list2.next;
+                }
+                op = op.next;
+            }
+
+            op.next = list1 ?? list2;
+            return dummy.next;
+        }
+        public void test()
+        {
+            //[1,2,4], list2 = [1,3,4]
+            var list1 = new ListNode();
+            list1.val = 1;
+            list1.next = new ListNode();
+            list1.next.val = 2;
+            list1.next.next = new ListNode();
+            list1.next.next.val = 4;
+            var list2 = new ListNode();
+            list2.val = 1;
+            list2.next = new ListNode();
+            list2.next.val = 3;
+            list2.next.next = new ListNode();
+            list2.next.next.val = 4;
+            MergeTwoLists(list1, list2);
+        }
+    }
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+
+    }
+}
